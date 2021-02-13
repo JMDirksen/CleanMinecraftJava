28102bef8862e562f17b9db44d80dadf177e7da4
Jefta Dirksen <JeftaDirksen@gmail.com>
2021-02-13 10:20:40 +0100

    Mem 6GB

--- Start.cmd
+++ Start.cmd
@@ -2,6 +2,6 @@
 title Clean Minecraft Java
 if exist mcstop.tmp del mcstop.tmp
 :loop
-start /abovenormal /wait /b java -Xms4G -Xmx4G -jar server.jar nogui
+start /abovenormal /wait /b java -Xms6G -Xmx6G -jar server.jar nogui
 if not exist mcstop.tmp goto loop
 del mcstop.tmp
