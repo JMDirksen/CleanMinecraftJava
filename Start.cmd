ddbbfc028eb60f825d06b00b4e70b24285672ca0
Jefta Dirksen <JeftaDirksen@gmail.com>
2021-02-28 11:47:31 +0100

    Use G1 Garbage Collector

--- Start.cmd
+++ Start.cmd
@@ -2,6 +2,6 @@
 title Clean Minecraft Java
 if exist mcstop.tmp del mcstop.tmp
 :loop
-start /abovenormal /wait /b java -Xms6G -Xmx6G -jar server.jar nogui
+start /abovenormal /wait /b java -Xmx6G -XX:+UseG1GC -jar server.jar nogui
 if not exist mcstop.tmp goto loop
 del mcstop.tmp
